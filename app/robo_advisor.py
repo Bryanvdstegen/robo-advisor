0e2b5e8edd7af36064a163cbe4d1efa50de09ef7
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-06-21 23:16:46 -0400

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,12 @@
 import requests
 import json
 
+# utility function to convert float or integer to USD-formatted string (for printing)
+# 
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price) #>12,000.71
+
 #
 # INFO REQUESTS
 #
@@ -20,6 +26,9 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
+
+
 # breakpoint()
 
 
@@ -30,10 +39,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #TODO: Program the daytime module
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
